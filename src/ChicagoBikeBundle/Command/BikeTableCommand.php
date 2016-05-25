61e8b8c4c83f6d353aa8b18c1141de690d440689
Tobias Schmidt <tobias.schmidt403+github@gmail.com>
2016-05-25 17:51:11 +0200

    add BikeTableCommand

---
+++ src/ChicagoBikeBundle/Command/BikeTableCommand.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * @author Tobias Schmidt <tobias.schmidt@refund-suisse.ch>
+ */
+
+namespace ChicagoBikeBundle\Command;
+
+use ChicagoBikeBundle\Entity\Bike;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BikeTableCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this->setName('chicago:import:bike')
+            ->setDescription('Fills bike table (dont ask how)');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $connection = $this->getContainer()->get('doctrine.dbal.connection');
+        $connection->getConfiguration()->setSQLLogger(null);
+
+        $em = $this->getContainer()->get('doctrine.orm.entity_manager');
+        
+        $result = $connection->executeQuery("SELECT * FROM trip ORDER BY bikeid, endtime");
+
+        /** @var Bike $oldBike */
+        $oldBike = null;
+
+        $i = 0;
+        foreach ($result as $item) {
+            if ($oldBike) {
+                if ($oldBike->getBikeid() == $item['bikeid']) {
+                    $oldBike->setEndtime($item['starttime']);
+                }
+                $em->persist($oldBike);
+                if ($i == 1000) {
+                    $output->writeln($oldBike->getBikeid() . " - " . $oldBike->getStarttime()->format("Y-m-d H:i:s"));
+                    $i = 0;
+                    $em->flush();
+                    $em->clear();
+                }
+            }
+            $bike = new Bike();
+            $bike->setBikeid($item['bikeid']);
+            $bike->setStationid($item['tostation']);
+            $bike->setStarttime($item['endtime']);
+
+            $oldBike = $bike;
+            $i++;
+        }
+
+        $em->persist($oldBike);
+        $em->flush();
+    }
+
+}
