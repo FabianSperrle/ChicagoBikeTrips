6f59482fdc52def5a1282c73b5354b8a7d358ccf
Tobias Schmidt <tobias.schmidt403+github@gmail.com>
2016-06-08 17:49:59 +0200

    Add idleBikeCountAction

--- src/ChicagoBikeBundle/Controller/StationController.php
+++ src/ChicagoBikeBundle/Controller/StationController.php
@@ -5,6 +5,7 @@ namespace ChicagoBikeBundle\Controller;
 use ChicagoBikeBundle\Entity\Station;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class StationController extends Controller
 {
@@ -33,5 +34,29 @@ class StationController extends Controller
         $json = $serializer->serialize($station, 'json');
         
         return $this->render('ChicagoBikeBundle:station:list.html.twig', compact('json'));
+    }
+
+    /**
+     * @Route("/idle_bike_count/{time}", requirements={"time": "\d+"})
+     * @param $time
+     * @return JsonResponse
+     */
+    public function idleBikeCountAction($time)
+    {
+        $conn = $this->get('database_connection');
+
+        $stmt = $conn->prepare("SELECT s.id, coalesce(b.count, 0) AS count
+        FROM station s
+        LEFT JOIN (
+            SELECT stationid, COUNT(*) AS count
+            FROM bike b 
+            WHERE starttime < to_timestamp(:time) AND endtime > to_timestamp(:time) 
+            GROUP BY stationid
+        ) b
+        ON b.stationid = s.id");
+        $stmt->bindValue(':time', $time);
+        $stmt->execute();
+
+        return new JsonResponse($stmt->fetchAll());
     }
 }
